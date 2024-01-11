af5b857166c03b45a95f8865109907146812f3af
DC <bidc@ucsd.edu>
2024-01-10 18:33:58 -0800

    test case for teleport functionality

---
+++ tests/unit/MyComponent.spec.ts
@@ -0,0 +1,25 @@
+import { mount } from '@vue/test-utils'
+import MyComponent from '@/components/MyComponent.vue'
+import Signup from '@/components/Signup.vue'
+
+beforeEach(() => {
+  // create teleport target
+  const el = document.createElement('div')
+  el.id = 'modal'
+  document.body.appendChild(el)
+})
+
+afterEach(() => {
+  // clean up
+  document.body.outerHTML = ''
+})
+
+test('teleport', async () => {
+  const wrapper = mount(MyComponent)
+
+  const signup = wrapper.getComponent(Signup)
+  await signup.get('input').setValue('valid_username')
+  await signup.get('form').trigger('submit.prevent')
+
+  expect(signup.emitted().signup[0]).toEqual(['valid_username'])
+})
